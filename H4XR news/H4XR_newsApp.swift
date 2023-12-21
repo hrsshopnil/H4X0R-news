04bec05305ca6be639cbe2908fa4834d3cd0f2c7
Habibur Rahman <hrsshainik@gmail.com>
2023-12-21 22:41:42 +0600

    Initial Commit

---
+++ H4XR news/H4XR_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XR_newsApp.swift
+//  H4XR news
+//
+//  Created by shopnil hasan on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct H4XR_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
